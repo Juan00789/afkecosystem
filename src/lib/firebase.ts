bcc191e3bae12cb452b437e555c7b037343ec4f7
Juan Ismael Alcantara Rodriguez <afkmulticervices@gmail.com>
2025-07-17 20:53:34 +0000

    This page isn’t working right now your_auth_domain_here didn’t send any

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,6 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -12,8 +11,18 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
+// Check for missing environment variables
+if (!firebaseConfig.apiKey) {
+    throw new Error("Missing NEXT_PUBLIC_FIREBASE_API_KEY in .env file");
+}
+if (!firebaseConfig.authDomain) {
+    throw new Error("Missing NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN in .env file");
+}
+if (!firebaseConfig.projectId) {
+    throw new Error("Missing NEXT_PUBLIC_FIREBASE_PROJECT_ID in .env file");
+}
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
