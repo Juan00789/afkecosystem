ebae0e0aa723df8ce181aeee6da7a8d134d2e5a6
Juan Ismael Alcantara Rodriguez <afkmulticervices@gmail.com>
2025-07-18 23:14:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/services/page.tsx
+++ src/app/dashboard/services/page.tsx
@@ -43,6 +43,14 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
